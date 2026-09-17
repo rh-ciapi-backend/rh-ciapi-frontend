81c13d65a457772bc1dfa55e75b7165b9ae6df56
rh-ciapi-backend <redeciapi@gmail.com>
2026-09-16 21:55:02 -0400

    Create saeUsuarioForm.ts

---
+++ src/sae/types/saeUsuarioForm.ts
@@ -0,0 +1,39 @@
+export interface SaeUsuarioForm {
+  prontuario: string;
+  turno: 'MANHÃ' | 'TARDE' | '';
+  nome: string;
+  sexo: string;
+  nacionalidade: string;
+
+  rg: string;
+  cpf: string;
+  dataNascimento: string;
+  cartaoSus: string;
+
+  dataIngresso: string;
+  situacaoCadastral: 'ATIVO' | 'INATIVO';
+
+  raca: string;
+  possuiDeficiencia: boolean;
+  tipoDeficiencia: string;
+  observacao: string;
+
+  endereco: {
+    logradouro: string;
+    numero: string;
+    complemento: string;
+    bairro: string;
+    cidade: string;
+    uf: string;
+    cep: string;
+  };
+
+  contatos: Array<{
+    telefone: string;
+    nomeContato: string;
+    parentesco: string;
+    tipo: string;
+    observacao: string;
+    principal: boolean;
+  }>;
+}
